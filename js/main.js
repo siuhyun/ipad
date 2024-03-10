328b5823ccac9aabd322844929266a68db4ac373
Kimcode <sophy0219@naver.com>
2024-03-10 16:06:51 +0900

    프로젝트 시작

---
+++ js/main.js
@@ -0,0 +1,202 @@
+import ipads from '../data/ipads.js';
+import navigations from '../data/navigations.js';
+
+// 장바구니!!
+const basketStarterEl = document.querySelector('header .basket-starter');
+const basketEl = basketStarterEl.querySelector('header .basket');
+
+basketStarterEl.addEventListener('click', function(event) {
+  event.stopPropagation();
+  if (basketEl.classList.contains('show')) { //hide
+    hideBasket();
+  } else { //show
+    showBasket();
+  }
+})
+
+basketEl.addEventListener('click', function(event) {
+  event.stopPropagation();
+})
+
+window.addEventListener('click', function() {
+  hideBasket();
+})
+
+function showBasket() {  
+  basketEl.classList.add('show');
+}
+function hideBasket() {  
+  basketEl.classList.remove('show');
+}
+
+//검색!!
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')] //배열로 얕은 복사
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', function (event) {  
+  event.stopPropagation(); //이벤트 버블링 스탑
+  hideSearch()
+});
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  stopScroll();
+  headerMenuEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  })
+  searchDelayEls.forEach(function(el, index) {
+    el.style.transitionDelay = index *.4 / searchDelayEls.length +'s';
+  })
+  setTimeout(function() {
+    searchInputEl.focus()
+  }, 600)
+}
+function hideSearch() {  
+  headerEl.classList.remove('searching');
+  playScroll()
+  headerMenuEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  })
+  searchDelayEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = index *.4 / searchDelayEls.length +'s';
+  })
+  searchDelayEls.reverse()
+  searchInputEl.value = ''; //초기화된다
+}
+
+function playScroll() {
+  document.documentElement.classList.remove('fixed');
+}
+function stopScroll() {
+  document.documentElement.classList.add('fixed');
+}
+
+// 헤더 메뉴 토글
+const menuStarterEl = document.querySelector('header .menu-starter');
+menuStarterEl.addEventListener('click',function() {
+  if(headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing');
+    searchInputEl.value = ''
+    playScroll()
+  } else {
+    headerEl.classList.add('menuing')
+    stopScroll()
+  }
+})
+
+// 헤더 검색!
+const searchTextFieldEl = document.querySelector('header .textfield')
+const searchCancelEl = document.querySelector('header .search-canceler')
+
+searchTextFieldEl.addEventListener('click', function() {
+  headerEl.classList.add('searching--mobile')
+  searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click', function() {
+  headerEl.classList.remove('searching--mobile')
+})
+
+// 화면 사이즈
+window.addEventListener('resize', function() {
+  if(window.innerWidth <= 740) {
+    headerEl.classList.remove('searching')
+  } else {
+    headerEl.classList.remove('searching--mobile')
+  }
+})
+
+//요소의 가시성 관찰
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return
+    }
+    entry.target.classList.add('show');
+  })
+})
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function(el) {
+  io.observe(el)
+})
+
+// 비디오 재생!
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click', function() {
+  video.play() //실제로 재생
+  playBtn.classList.add('hide') //재생버튼 숨기기
+  pauseBtn.classList.remove('hide') 
+})
+pauseBtn.addEventListener('click', function() {
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
+
+// '당신에게 맞는 iPad는?' 렌더링
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(function(ipad) {
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('item')
+
+  let colorList = ''
+  ipad.colors.forEach(function(color) {
+    colorList += `<li style="background-color: ${color};"></li>`
+  })
+
+
+  itemEl.innerHTML = /* html */`
+  <div class="thunbnail">
+    <img src="${ipad.thumbnail}" alt="${ipad.name}"/>
+  </div>
+  <ul class="colors">
+    ${colorList}
+  </ul>
+  <h3 class="name">${ipad.name}</h3>
+  <p class="tagline">${ipad.tagline}</p>
+  <p class="price">￦${ipad.price.toLocaleString('en-US')}부터</p>
+  <button class="btn">구입하기</button>
+  <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
+
+  itemsEl.append(itemEl)
+})
+
+// 푸터 내비게이션 맵 랜더링!
+const navigationsEl = document.querySelector('footer .navigations')
+navigations.forEach(nav => {
+  const mapEl = document.createElement('div')
+  mapEl.classList.add('map')
+
+  let mapList = ''
+  nav.maps.forEach(map => {
+    mapList += /* html */ `<li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`
+  })
+
+  mapEl.innerHTML = /* html */ `
+    <h3>
+      <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `
+
+  navigationsEl.append(mapEl)
+})
+
+const thisYearEl = document.querySelector('span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
